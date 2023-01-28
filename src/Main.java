4f9511d3ec0738c1e337aad8fb955286f14d0db5
Netologia test <vizalyyy@gmail.com>
2023-01-28 22:34:05 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int costTicket = 155;
+        int costBonus = 20;
+
+        int allBonus = costTicket / costBonus;
+
+        System.out.println(allBonus);
+    }
+}
